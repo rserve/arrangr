e6a5e834211e9bed6ddd988fc48ac9c9bb799ef6
Carl Grundberg <carl.grundberg@menmo.se>
2013-05-27 21:29:16 +0200

    form to create groups

--- client/js/app/controllers/GroupsView.js
+++ client/js/app/controllers/GroupsView.js
@@ -4,6 +4,13 @@ define(['framework/logger'], function (logger) {
 
 	var Controller = function ($scope, $http) {
 
+        $scope.create = function(group) {
+            $http.post('api/groups', group).success(function(data) {
+                $scope.groups.push(data);
+                group.name = '';
+            });
+        };
+
 		$http.get('api/groups').success(function (data) {
 			logger.log('GroupsView - data received', data);
 			$scope.groups = data;
