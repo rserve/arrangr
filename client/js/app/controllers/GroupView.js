048ea657395e1b1a02c6b2c17ff65a85cb865542
Robert Ödling <robert.odling@gmail.com>
2013-06-02 20:30:47 +0200

    testing pluralization directive

--- client/js/app/controllers/GroupView.js
+++ client/js/app/controllers/GroupView.js
@@ -2,7 +2,7 @@ define(['framework/logger'], function (logger) {
 
 	'use strict';
 
-	var Controller = function ($scope, $http, $location, $routeParams, groupsService) {
+	var Controller = function ($scope, $filter, $location, $routeParams, groupsService) {
 
 		var id = $routeParams.groupId,
 			service = groupsService;
@@ -12,6 +12,7 @@ define(['framework/logger'], function (logger) {
 				success(function (data) {
 					$scope.group = data;
 					$scope.status = 'info';
+
 				}).
 				error(function (data) {
 					$scope.message = "Server says '" + data.error + "'";
@@ -64,11 +65,27 @@ define(['framework/logger'], function (logger) {
 		$scope.join = joinGroup;
 
 		getGroup();
+
+		$scope.groupCount = {
+			0: 'No one have joined',
+			1: 'One person have joined.',
+			2: 'Two persons have joined.',
+			3: 'Three persons have joined.',
+			4: 'Four persons have joined.',
+			5: 'Five persons have joined.',
+			6: 'Six persons have joined.',
+			7: 'Seven persons have joined.',
+			8: 'Eight persons have joined.',
+			9: 'Nine persons have joined.',
+			10: 'Ten persons have joined.',
+			other: '{} persons have joined.'
+		};
+
 	};
 
 
 	//inject dependencies
-	Controller.$inject = ['$scope', '$http', '$location', '$routeParams', 'groupsService'];
+	Controller.$inject = ['$scope', '$filter', '$location', '$routeParams', 'groupsService'];
 
 	//export
 	return Controller;
