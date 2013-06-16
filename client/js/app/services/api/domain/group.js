36698dd414965a758dd8b5c2a40dac03dae22ac7
Robert Ödling <robert.odling@gmail.com>
2013-06-16 19:39:44 +0200

    missing files

---
+++ client/js/app/services/api/domain/group.js
@@ -0,0 +1,29 @@
+/*
+ * Register controllers
+ * */
+
+define(function (require, exports, module) {
+
+	'use strict';
+
+	var _ = require('underscore');
+
+	var Group = function (data) {
+		_.extend(this, data);
+	};
+
+	var proto = Group.prototype;
+
+	proto.isAdmin = function (user) {
+
+		for (var i = 0, len = this.members.length; i < len; i++) {
+			var member = this.members[i];
+			if (member.user === user._id && member.admin) {
+				return true;
+			}
+		}
+		return false;
+	};
+
+	module.exports = Group;
+});
---
+++ client/js/app/services/api/requestBuilder.js
@@ -0,0 +1,86 @@
+define(['underscore'], function (_) {
+
+	'use strict';
+
+	var RequestBuilder = function () {
+		this.path = [];
+	};
+
+	var proto = RequestBuilder.prototype;
+
+
+	proto.setMethod = function (method) {
+		this.method = method;
+		return this;
+	};
+
+
+	proto.setUrl = function (url) {
+		this.url = url;
+		return this;
+	};
+
+	proto.addPath = function (path) {
+
+		this.path.push(path);
+
+		return this;
+	};
+
+	proto.setData = function (data) {
+		this.data = data;
+		return this;
+	};
+
+	proto.setSuccessCb = function (cb) {
+		this.success = cb;
+		return this;
+	};
+
+	proto.setErrorCb = function (cb) {
+		this.error = cb;
+		return this;
+	};
+
+	proto.addResponseMiddleware = function (fn) {
+		this.respMiddleware = fn;
+		return this;
+	};
+
+	proto.build = function () {
+
+		var _this,
+			fullUrl = this.url;
+
+		//add paths to full url
+		if (this.path.length > 0) {
+			fullUrl += '/' + this.path.join('/');
+		}
+
+		var successCb;
+
+
+		if (this.respMiddleware) {
+			successCb = _.bind(function (data) {
+				this.success(this.respMiddleware(data));
+			}, this);
+
+		} else {
+			successCb = this.success;
+		}
+
+		return {
+			url: fullUrl,
+			method: this.method,
+			data: this.data,
+			success: successCb,
+			error: this.error
+
+		}
+	};
+
+
+	//export
+	return RequestBuilder;
+
+});
