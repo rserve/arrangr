64ef1b5cb1d085c2a38bfcc97b4ba43ae96dc59b
Carl Grundberg <carl.grundberg@menmo.se>
2013-06-26 17:29:13 +0200

    Specs for user

--- server/controllers/errorhandler.js
+++ server/controllers/errorhandler.js
@@ -1,10 +1,14 @@
 module.exports = function(err, res, msg) {
     if(err) {
-        console.log(msg, err);
-        res.status(500).send({
-            message: msg,
-            error: err
-        });
+        if(err.name == 'ValidationError') {
+            res.status(400).send(err);
+        } else {
+            console.log(msg, err);
+            res.status(500).send({
+                message: msg,
+                error: err
+            });
+        }
         return true;
     }
     return false;
--- server/specs/users.spec.js
+++ server/specs/users.spec.js
@@ -45,31 +45,73 @@ describe(usersEndpoint, function () {
     describe('not authenticated', function () {
 
         describe('get /logout', function () {
-            helper.testUnauthorized(function(cb) {
+            helper.testUnauthorized(function (cb) {
                 request(usersEndpoint + '/logout', cb);
             });
         });
 
         describe('get /session', function () {
-            helper.testUnauthorized(function(cb) {
+            helper.testUnauthorized(function (cb) {
                 request(usersEndpoint + '/session', cb);
             });
         });
 
         describe('get /:userId', function () {
-            helper.testUnauthorized(function(cb) {
+            helper.testUnauthorized(function (cb) {
                 request(usersEndpoint + '/' + testUsers[0].id, cb);
+            });
+        });
+
+        describe('post', function () {
+            var testUser = { email: 'test3@email.com', password: 'password' };
+            it('should create user', function (done) {
+                request.post(usersEndpoint, { form: testUser }, function (err, resp, responseUser) {
+                    expect(err).toBeFalsy();
+                    expect(resp.statusCode).toEqual(200);
+                    expect(responseUser.email).toEqual(testUser.email);
+                    User.find(function (err, users) {
+                        expect(err).toBeFalsy();
+                        expect(users.length).toEqual(testData.users.length + 1);
+                        done();
+                    });
+                });
+            });
+
+            it('should not allow existing email', function (done) {
+                request.post(usersEndpoint, { form: testData.users[0] }, function (err, resp) {
+                    expect(err).toBeFalsy();
+                    expect(resp.statusCode).toEqual(400);
+                    done();
+                });
+            });
+
+            it('should not allow empty email', function (done) {
+                request.post(usersEndpoint, { form: { email: '', password: 'password' } }, function (err, resp) {
+                    expect(err).toBeFalsy();
+                    expect(resp.statusCode).toEqual(400);
+                    done();
+                });
+            });
+
+            it('should not allow empty password', function (done) {
+                request.post(usersEndpoint, { form: { email: 'test3@email.com', password: '' } }, function (err, resp) {
+                    expect(err).toBeFalsy();
+                    expect(resp.statusCode).toEqual(400);
+                    done();
+                });
             });
         });
     });
 
     describe('authenticated', function () {
 
+        var authenticatedUser = testData.users[0];
+
         beforeEach(function () {
             var done = false;
 
             runs(function () {
-                helper.login(testData.users[0].email, testData.users[0].password, function () {
+                helper.login(authenticatedUser.email, authenticatedUser.password, function () {
                     done = true;
                 });
             });
@@ -79,16 +121,39 @@ describe(usersEndpoint, function () {
             });
         });
 
-        describe('get /logout', function() {
-           it('should logout the user', function(done) {
-                request(usersEndpoint + '/logout', function(err, resp) {
+        describe('get /logout', function () {
+            it('should logout the user', function (done) {
+                request(usersEndpoint + '/logout', function (err, resp) {
                     expect(err).toBeFalsy();
                     expect(resp.statusCode).toEqual(200);
-                    request(usersEndpoint + '/session', function(err, resp) {
+                    request(usersEndpoint + '/session', function (err, resp) {
                         expect(err).toBeFalsy();
                         expect(resp.statusCode).toEqual(401);
                         done();
                     });
+                });
+            });
+        });
+
+        describe('get /session', function () {
+            it('should return logged in user', function (done) {
+                request(usersEndpoint + '/session', function (err, resp, user) {
+                    expect(err).toBeFalsy();
+                    expect(resp.statusCode).toEqual(200);
+                    expect(user.email).toEqual(authenticatedUser.email);
+                    done();
+                });
+            });
+        });
+
+        describe('get /:userId', function () {
+            it('should return correct user', function (done) {
+                var testUser = testUsers[1];
+                request(usersEndpoint + '/' + testUser.id, function(err, resp, user) {
+                    expect(err).toBeFalsy();
+                    expect(resp.statusCode).toEqual(200);
+                    expect(user.email).toEqual(testUser.email);
+                    done();
                 });
             });
         });
